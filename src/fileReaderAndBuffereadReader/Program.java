e00880abfd5ce28b6e73b74ee7eb80ec22cfee22
Dowglas Maia <dowglasmaia@gmail.com>
2019-07-18 11:20:18 -0300

    Melhorando o codigo, da leitura com BuffereReader

--- src/fileReaderAndBuffereadReader/Program.java
+++ src/fileReaderAndBuffereadReader/Program.java
@@ -10,38 +10,16 @@ public class Program {
 
 		String path = "c:\\temp\\int.txt";
 
-		FileReader fr = null;
-
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
-			String line = br.readLine();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {		
+				String line = br.readLine();
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
 
 		} catch (IOException e) {
-
 			System.out.println("Error: " + e.getMessage());
-
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException err) {
-				err.printStackTrace();
-			}
-
-		}
-
+		} 
 	}
 
 }
